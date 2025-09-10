b091f56f11d0d431350c6e64c15f29c270f05088
anastasiasemenovna <zyxfzsddc09@gmail.com>
2025-09-10 21:51:19 +0300

    stackjava.java

--- stackjava.java
+++ stackjava.java
@@ -1,1 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Stack;
 
+class Codech{
+	public static void main (String[] args){
+		Stack<Integer> stack = new Stack<Integer>();
+	}
+}
