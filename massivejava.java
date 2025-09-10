858ea19cb7ddf986a0124275af4994249e375946
anastasiasemenovna <zyxfzsddc09@gmail.com>
2025-09-10 22:44:17 +0300

    massivejava.java

--- massivejava.java
+++ massivejava.java
@@ -1,1 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+class Codech{
+	public static void main (String[] args){
+		int[] marks = new int[10];
+		System.out.println(marks.length);
+	}
+}
